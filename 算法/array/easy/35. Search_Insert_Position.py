d45bc3186435c5421f7f09884befc3f0fa09fbf9
Yonghong <39487436+LYH66@users.noreply.github.com>
2019-10-27 11:25:11 +0800

    Update 35. Search_Insert_Position.py

--- 算法/array/easy/35. Search_Insert_Position.py
+++ 算法/array/easy/35. Search_Insert_Position.py
@@ -61,5 +61,13 @@ class Solution:
             if nums[index] > target:
                 return index
         return n
- 
 
+#method_4   index(), sorted()
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums = sorted(nums)
+            return nums.index(target)
