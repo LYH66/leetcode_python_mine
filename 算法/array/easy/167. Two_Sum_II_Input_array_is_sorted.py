c0b2a30172f9223c7b34a919b9121d3267d047c7
Yonghong <39487436+LYH66@users.noreply.github.com>
2019-11-04 10:36:51 +0800

    Update 167. Two_Sum_II_Input_array_is_sorted.py

--- 算法/array/easy/167. Two_Sum_II_Input_array_is_sorted.py
+++ 算法/array/easy/167. Two_Sum_II_Input_array_is_sorted.py
@@ -13,7 +13,24 @@ Output: [1,2]
 Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 """
 
-#method_1
+######  重点在于如何利用数组已按升序排序这一特点  ######
 
+#method_1     80ms  时空复杂度：O(n), O(1)
+"""
+我们使用两个指针，初始分别位于第一个元素和最后一个元素位置，比较这两个元素之和与目标值的大小。如果和等于目标值，我们发现了这个唯一解。
+如果比目标值小，我们将较小元素指针增加一。如果比目标值大，我们将较大指针减小一。移动指针后重复上述比较知道找到答案。
+"""
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers) - 1
+        while i < j:
+            Sum = numbers[i] + numbers[j]
+            if Sum > target:
+                j -= 1
+            elif Sum < target:
+                i += 1
+            else:
+                return i+1, j+1
 
 
