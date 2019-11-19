c1c776ae5203006d14d2960dc695bd42c2dafad7
Yonghong <39487436+LYH66@users.noreply.github.com>
2019-11-19 14:22:39 +0800

    Update 414. Third_Maximum_Number.py

--- 算法/array/easy/414. Third_Maximum_Number.py
+++ 算法/array/easy/414. Third_Maximum_Number.py
@@ -21,3 +21,38 @@ Output: 1
 Explanation: Note that the third maximum here means the third maximum distinct number.
 Both numbers with value 2 are both considered as second maximum.
 """
+
+# method_1    利用python特性，去重、排序（74%）
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = sorted(list(set(nums)))
+        n = len(nums)
+        if n < 3:
+            return nums[n-1]
+        else:
+            return nums[n-3]
+
+# method_2    暴力解法，三次循环（74%）
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        first = second = third = float("-inf")
+        flag = 0
+
+        for i in nums:
+            if i>= first:
+                first = i
+        for i in nums:
+            if i>= second and i < first:
+                second = i
+        
+        for i in nums:
+            if i>= third and i < second:
+                third = i
+                flag = 1
+        
+        if flag == 1:
+            return third
+        else:
+            return first
+
+
