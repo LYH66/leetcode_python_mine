423c2f85f53212597dcc8eccbc498b1fe1f09953
Yonghong <39487436+LYH66@users.noreply.github.com>
2019-12-04 10:27:36 +0800

    Update 724. Find_Pivot_Index.py

--- 算法/array/easy/724. Find_Pivot_Index.py
+++ 算法/array/easy/724. Find_Pivot_Index.py
@@ -41,6 +41,15 @@ class Solution:
             leftsum+=nums[i]
         return -1
 
-
+# method_2
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        left_sum = 0
+        for i, num in enumerate(nums):
+            if left_sum == (total - num) / 2:
+                return i
+            left_sum += num
+        return -1
 
 
